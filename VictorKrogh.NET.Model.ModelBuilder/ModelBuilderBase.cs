ec5cfd1cab0d5bce8d3671225b15a94f20d2350a
KingoBoiii <kingodk@outlook.dk>
2023-04-18 17:30:35 +0200

    Added VictorKrogh.NET.Extensions.Linq as dependency

--- VictorKrogh.NET.Model.ModelBuilder/ModelBuilderBase.cs
+++ VictorKrogh.NET.Model.ModelBuilder/ModelBuilderBase.cs
@@ -6,15 +6,4 @@ public abstract class ModelBuilderBase<TIn, TOut> : IModelBuilder<TIn, TOut>
     public abstract Task<TOut> BuildAsync(TIn input);
 }
 
-public static class AsyncEnumerableExtensions
-{
-    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> items, CancellationToken cancellationToken = default)
-    {
-        var list = new List<T>();
-        await foreach (var item in items.WithCancellation(cancellationToken).ConfigureAwait(false))
-        {
-            list.Add(item);
-        }
-        return list;
-    }
-}
+
